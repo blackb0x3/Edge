bb7f2fc629f8d535b59ff7e6a75ce1b2adf9f242
Luke Barker <BarkerLJ@cardiff.ac.uk>
2018-10-29 21:41:26 +0000

    Add tiles lib
     * includes boilerplate tile classes
       - prefills letter and it's points

---
+++ models/tiles.py
@@ -0,0 +1,134 @@
+class ScrabbleTile:
+    def __init__(self, letter: str, points: int):
+        self.letter = letter
+        self.points = points
+
+
+class TileA(ScrabbleTile):
+    def __init__(self):
+        super().__init__("A", 1)
+
+
+class TileB(ScrabbleTile):
+    def __init__(self):
+        super().__init__("B", 3)
+
+
+class TileC(ScrabbleTile):
+    def __init__(self):
+        super().__init__("C", 3)
+
+
+class TileD(ScrabbleTile):
+    def __init__(self):
+        super().__init__("D", 2)
+
+
+class TileE(ScrabbleTile):
+    def __init__(self):
+        super().__init__("E", 1)
+
+
+class TileF(ScrabbleTile):
+    def __init__(self):
+        super().__init__("F", 4)
+
+
+class TileG(ScrabbleTile):
+    def __init__(self):
+        super().__init__("G", 2)
+
+
+class TileH(ScrabbleTile):
+    def __init__(self):
+        super().__init__("H", 4)
+
+
+class TileI(ScrabbleTile):
+    def __init__(self):
+        super().__init__("I", 1)
+
+
+class TileJ(ScrabbleTile):
+    def __init__(self):
+        super().__init__("J", 8)
+
+
+class TileK(ScrabbleTile):
+    def __init__(self):
+        super().__init__("K", 5)
+
+
+class TileL(ScrabbleTile):
+    def __init__(self):
+        super().__init__("L", 1)
+
+
+class TileM(ScrabbleTile):
+    def __init__(self):
+        super().__init__("M", 3)
+
+
+class TileN(ScrabbleTile):
+    def __init__(self):
+        super().__init__("N", 1)
+
+
+class TileO(ScrabbleTile):
+    def __init__(self):
+        super().__init__("O", 1)
+
+
+class TileP(ScrabbleTile):
+    def __init__(self):
+        super().__init__("P", 3)
+
+
+class TileQ(ScrabbleTile):
+    def __init__(self):
+        super().__init__("Q", 10)
+
+
+class TileR(ScrabbleTile):
+    def __init__(self):
+        super().__init__("R", 1)
+
+
+class TileS(ScrabbleTile):
+    def __init__(self):
+        super().__init__("S", 1)
+
+
+class TileT(ScrabbleTile):
+    def __init__(self):
+        super().__init__("T", 1)
+
+
+class TileU(ScrabbleTile):
+    def __init__(self):
+        super().__init__("U", 1)
+
+
+class TileV(ScrabbleTile):
+    def __init__(self):
+        super().__init__("V", 4)
+
+
+class TileW(ScrabbleTile):
+    def __init__(self):
+        super().__init__("W", 4)
+
+
+class TileX(ScrabbleTile):
+    def __init__(self):
+        super().__init__("X", 8)
+
+
+class TileY(ScrabbleTile):
+    def __init__(self):
+        super().__init__("Y", 4)
+
+
+class TileZ(ScrabbleTile):
+    def __init__(self):
+        super().__init__("Z", 10)
